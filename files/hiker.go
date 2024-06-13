55a7265e040627ecc3b920245a6f1fa8800b3682
elbtLM <elbtLM@cyber-dojo.org>
2024-06-13 09:03:38 +0000

    3 ran tests, predicted green, got green

--- files/hiker.go
+++ files/hiker.go
@@ -1,5 +1,14 @@
 package reverse_romans
 
+import (
+    "strings"
+)
+
 func Arabic(roman string) (arabic int) {
+    for strings.HasPrefix(roman, "M") {
+        arabic += 1000
+        roman = roman[1:]
+    }
+    
     return
 }
